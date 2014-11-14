5c5bf0906d2336721d3851740b8f70ebf346f24d
mtotho <mtotho@gmail.com>
2014-11-13 19:57:13 -0500

    Updated navbar controller to indicate which tab is pressed

--- client/app/controllers/navbar/navbar.controller.js
+++ client/app/controllers/navbar/navbar.controller.js
@@ -1,6 +1,55 @@
 'use strict';
 
-angular.module('placemapApp')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.message = 'Hello';
+angular.module('placemapApp').controller('NavbarCtrl', function ($scope, $location) {
+    
+
+	$scope.$on('$stateChangeSuccess', function () {
+			
+				/*
+			if(auth.isLoggedIn()){
+				$scope.user=auth.getUser();
+				$scope.isLoggedIn=true;
+			}else{
+				$scope.isLoggedIn=false;
+			}
+			*/
+
+			var path=$location.path().split("/")[1];
+
+			$("header .nav li").removeClass("active");
+			$scope.userNavText="Admin Panel";
+
+			switch(path){
+				case "":{
+					
+					$("header .nav li:nth-child(1)").addClass("active");
+					break;
+				}
+				case "studyarea":{
+					$("header .nav li:nth-child(2)").addClass("active");
+
+					break;
+				}
+				case "login":{
+						
+					$("header .nav li:nth-child(3)").addClass("active");
+					break;
+				}
+				case "admin":{
+					$scope.userNavText="Logout";
+					$("header .nav li:nth-child(4)").addClass("active");
+
+					break;
+				}
+				default:{
+					;
+					break;
+				}
+			}
+
+			console.log(path);
+
+		});
+
+
   });
