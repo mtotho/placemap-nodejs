640556e4ae27257453538d0db70199ed148f7003
mtotho <mtotho@gmail.com>
2014-11-18 11:07:14 -0500

    [put][audit_type] can now handle questions that are ids not objects

--- server/api/audit_type/audit_type.controller.js
+++ server/api/audit_type/audit_type.controller.js
@@ -36,22 +36,29 @@ exports.update = function(req, res) {
     if(!audit_type) { return res.send(404); }
     
     audit_type.name=req.body.name;
+
+    //Wipe out questions Array so we can rewrite
     audit_type.questions = new Array();
 
+    //Loop over the update questions
     for(var q in req.body.questions){
-   // console.log(q);
+      
+      //If question comes in as object
       if(req.body.questions[q].question._id != null){
 
-            audit_type.questions.push({
+          audit_type.questions.push({
+            order: req.body.questions[q].order,
+            question: req.body.questions[q].question._id
+          });
+
+      //If question comes in as id
+      }else{
+          audit_type.questions.push({
               order: req.body.questions[q].order,
-              question: req.body.questions[q].question._id
-            });
+              question: req.body.questions[q].question
+          });
       }
-
-    
-    }
-
-   // 
+    }//end for
 
     var updated = audit_type;//_.merge(audit_type, req.body);
     updated.save(function (err) {
